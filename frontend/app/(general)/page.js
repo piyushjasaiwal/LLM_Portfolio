9fa92885e1d4cad36b571534884edc230c92a189
Piyush Jasaiwal <coderpiyush123@gmail.com>
2025-11-29 06:29:24 +0530

    changing padding of the main page

--- frontend/app/(general)/page.js
+++ frontend/app/(general)/page.js
@@ -5,8 +5,13 @@ import ChatIframe from '../../components/ChatIframe'
 
 export default function Home() {
   return (
-    <motion.div initial={{ opacity: 0, y: 8 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }}>
-      
+    <motion.div
+      initial={{ opacity: 0, y: 8 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+      className="pb-20"
+    >
       <div className="mt-8 grid grid-cols-1 lg:grid-cols-2 gap-6">
         <div className="lg:col-span-1">
           <Hero />
